91ec0c32ac358d3016f5626a228226f3d626e29b
konradroesler <konrad.rosler@gmail.com>
2024-09-08 17:43:35 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	description = "NixOs configuration";
+
+	inputs = {
+		nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+		# home-manager, used for managing user configuration
+		home-manager = {
+			url = "github:nix-community/home-manager/release-24.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
+		nixosConfigurations = {
+			# 'nixos' is the hostname
+			nixos = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+
+					# make home-manager as a module of nix so that 
+					# home-manager configuration will be deployed automatically
+					# when executing `nixos-rebuild switch`
+					home-manager.nixosModules.home-manager 
+					{
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+						home-manager.users.konrad = import ./home.nix;
+					}
+				];
+			};
+		};
+	};
+}
---
+++ home.nix
@@ -0,0 +1,28 @@
+{ lib, config, pkgs, ... }:
+
+{
+	home.username = "konrad";
+	# force value, not doing so causes a conflict with `nixos/commons.nix`
+	home.homeDirectory = lib.mkForce "/home/konrad/";
+
+	# Packages that should be installed to the user profile
+	home.packages = with pkgs; [
+		neofetch
+	];
+
+	programs.git = {
+		enable = true;
+		userName = "konradroesler";
+		userEmail = "konrad.rosler@gmail.com";
+		extraConfig = {
+			init.defaultBranch = "main";
+		};
+	};
+
+	programs.bash.enable = true;
+
+	home.stateVersion = "24.05";
+
+	# Let home manager install and manage itself
+	programs.home-manager.enable = true;
+}
