88f9e200eb00b59f045b32f210ea7b5d970fddc2
konradroesler <konrad.rosler@gmail.com>
2024-09-09 01:09:01 +0200

    Fix args

--- hosts/nixos-gnome/firefox.nix
+++ hosts/nixos-gnome/firefox.nix
@@ -26,17 +26,17 @@
         DisplayBookmarksToolbar = "never"; # alternatives: 'always' or 'newtab'
         DisplayMenuBar = "default-off"; # alternatives: 'always', 'never' or 'default-on'
         SearchBar = "unified"; # alternatives: 'separate'
-      };
-
-      # Check about:support for extensions/add-on ID strings
-      # Valid string for installation mode are 'allowed', 'blocked',
-      # 'force_installed' and 'normal_installed'
-      ExtensionSettings = {
-        "*".installation_mode = "blocked"; # blocks all add ons except the ones specified below
-        # uBlock Origin:
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
+          
+        # Check about:support for extensions/add-on ID strings
+        # Valid string for installation mode are 'allowed', 'blocked',
+        # 'force_installed' and 'normal_installed'
+        ExtensionSettings = {
+          "*".installation_mode = "blocked"; # blocks all add ons except the ones specified below
+          # uBlock Origin:
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
         };
       };
     };
