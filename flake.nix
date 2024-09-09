f38fa5f8a959fab42f507329655dca2b27784e90
konradroesler <konrad.rosler@gmail.com>
2024-09-09 16:24:03 +0200

    Format

--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
 {
 	description = "NixOs configuration";
 
-	inputs = {
-		nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
-		# home-manager, used for managing user configuration
-		home-manager = {
-			url = "github:nix-community/home-manager/release-24.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+  inputs = { 
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+		home-manager.url = "github:nix-community/home-manager/release-24.05";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
