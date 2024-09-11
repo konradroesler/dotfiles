02c62f32d7723e19ed44ab6aa639434a3bfbbb3a
konradroesler <konrad.rosler@gmail.com>
2024-09-11 19:57:17 +0200

    Typo + kitty

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -4,6 +4,7 @@
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 
   programs.hyprland = {
     enable = true;
@@ -23,7 +24,7 @@
 
   environment.systemPackages = with pkgs; [
     hyprpaper
-    foot
+    kitty
     wofi
     waybar
   ];
