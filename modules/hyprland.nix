3a6746680354db565d5fa4d427feb32ef864a62c
konradroesler <konrad.rosler@gmail.com>
2024-09-12 02:22:44 +0200

    Configure keybinds

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,6 +1,11 @@
 {
+  wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
-    bind = [ "$mod, F, exec, firefox" ];
+    bind = [ 
+      "$mod, B, exec, firefox"
+      "$mod, Q, exec, kitty" 
+      "$mod, F, fullscreen"
+    ];
   };
 }
