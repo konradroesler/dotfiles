ff1222b4bf4075bf41dab4b1b271f2131bb6b770
konradroesler <konrad.rosler@gmail.com>
2024-09-13 23:04:47 +0200

    Limit number of generations

--- system/boot.nix
+++ system/boot.nix
@@ -3,7 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+	boot.loader.systemd-boot.configurationLimit = 10;
   # Essential for wayland/hyprland
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
