53d7f30654aabaca8e4d1edd55aa871009ea705f
konradroesler <konrad.rosler@gmail.com>
2024-09-14 11:31:52 +0200

    Add wayland module

--- system/services.nix
+++ system/services.nix
@@ -2,6 +2,7 @@
 { pkgs, username, ... }:
 
 {
+	services.dbus.enable = true;
   # Brightness control (wayland)
   services.illum.enable = true;
   services.openssh.enable = true;
--- system/xserver.nix
+++ system/xserver.nix
@@ -1,9 +1,9 @@
-_:
+{ pkgs, username, ... }:
 
 {
   # Essential for wayland/hyprland
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+	services.xserver.displayManager.gdm.enable = true;
   # services.xserver.displayManager.gdm.wayland = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
   services.xserver.xkb.layout = "us";
