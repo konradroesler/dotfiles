3241f9684001e73483c2209412ccc0cddd30d9f1
konradroesler <konrad.rosler@gmail.com>
2025-11-25 15:09:47 +0100

    steam

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -1,20 +1,4 @@
 { pkgs, ... }:
 {
-	programs.steam = {
-		enable = true;
-		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-		localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-	};
-	nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-			"steam"
-			"steam-original"
-			"steam-unwrapped"
-			"steam-run"
-		];
-  hardware.graphics.extraPackages = with pkgs; [
-    vulkan-loader
-    vulkan-validation-layers
-    vulkan-extension-layer
-  ];
+	programs.steam.enable = true;
 }
